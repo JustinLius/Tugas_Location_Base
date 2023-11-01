4e00b9bbc7c8730cb08547d75e676ad5513184d7
JustinLius <liusjustin6@gmail.com>
2023-11-01 19:51:01 +0700

    first commit

---
+++ location_base/lib/weather.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+import 'location.dart';
+
+class WeatherService {
+
+  double? temperature;
+  String? weatherIcon;
+  String? cityName;
+
+  final API_KEY = "381b9bb09466bb50f8be3dc87ceb9c91";
+
+  Future<dynamic> getWeatherData() async {
+
+    LocationService s = LocationService();
+    await s.getCurrentLocation();
+
+    double? lat = s.latitude;
+    double? lon = s.longitude;
+
+    http.Response response = await http.get(Uri.parse(
+      "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$API_KEY&units=metric"));
+    
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var weatherData = jsonDecode(data);
+      temperature = weatherData['main']['temp'];
+      weatherIcon = weatherData['weather'][0]['icon'];
+      cityName = weatherData['name'];
+    }
+  }
+
+}
